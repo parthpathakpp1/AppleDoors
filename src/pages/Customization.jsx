82ee306907a2d23db58ef9deeccbf8e3654a969f
Harsh Deep <hdeep61034@gmail.com>
2023-07-31 15:14:17 +0530

    Routing Done

--- src/pages/Customization.jsx
+++ src/pages/Customization.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import Header from '../components/Header/Header';
 import './Customization.css';
+import { useParams } from 'react-router-dom';
 
 const Customization = () => {
+  const { doorName } = useParams();
   return (
+    <>
+    <Header />
     <div className="customization-page">
       <h2>Customization</h2>
-      <p>This is the customization page.</p>
+      <p>This is the customization page of {doorName}.</p>
     </div>
+    </>
   );
 };
 
