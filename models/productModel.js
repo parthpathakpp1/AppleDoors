cbdf48916c89885a61e8efbde30375f3382e1a69
parthpathakpp1 <85991449+parthpathakpp1@users.noreply.github.com>
2023-09-12 00:10:36 +0530

    cHANGES

--- models/productModel.js
+++ models/productModel.js
@@ -1,45 +1,49 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        slug: {
-            type: String,
-            required: true,
-        },
-        description: {
-            type: String,
-            required: true,
-        },
-        price: {
-            type: Number,
-            required: true,
-        },
-        category: {
-            type: mongoose.ObjectId,
-            ref: "Category",
-            required: true,
-        },
-        quantity: {
-            type: Number,
-            required: true,
-        },
-        photo: {
-            data: Buffer,
-            contentType: String,
-        },
-        secondPhoto: {
-            data: Buffer,
-            contentType: String,
-        },
-        shipping: {
-            type: Boolean,
-        },
+  {
+    name: {
+      type: String || Object,
+      required: true,
     },
-    { timestamps: true }
+    slug: {
+      type: String,
+    },
+    description: {
+      type: String || Object,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: mongoose.ObjectId,
+      ref: "Category",
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+    secondPhoto: {
+      data: Buffer,
+      contentType: String,
+    },
+    shipping: {
+      type: Boolean,
+    },
+    different: {
+      type: Boolean,
+    },
+    image_ids: {
+      type: Object,
+    },
+  },
+  { timestamps: true }
 );
 
 export default mongoose.model("Products", productSchema);
