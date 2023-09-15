221168ebd48fd85c501ea669f240f8b712084328
parthpathakpp1 <85991449+parthpathakpp1@users.noreply.github.com>
2023-09-15 22:51:34 +0530

    More Changes

--- client/src/pages/Admin/AdminDashboard.jsx
+++ client/src/pages/Admin/AdminDashboard.jsx
@@ -11,11 +11,11 @@ const AdminDashboard = () => {
     <>
       <Header />
 
-      <div className="dashboard">
-        <div className="col-md-3">
+      <div className="dashboard-container">
+        <div className="dashboard-sidebar">
           <AdminMenu />
         </div>
-        <div className="col-md-9">
+        <div className="dashboard-content">
         <div className="card">
                             <h3> Admin Name : {auth?.user?.name}</h3>
                             <h3> Admin Email : {auth?.user?.email}</h3>
--- client/src/pages/user/Dashboard.jsx
+++ client/src/pages/user/Dashboard.jsx
@@ -5,20 +5,18 @@ import './Dashboard.css'; // Import the CSS file
 import UserMenu from '../../components/UserMenu/UserMenu';
 import { useAuth } from '../../context/auth';
 
-
 const Dashboard = () => {
   const auth = useAuth();
+
   return (
     <>
       <Header />
 
-      <div className="dashboard">
-        <div className="col-md-3">
-          <div className="user-menu">
-            <UserMenu />
-          </div>
+      <div className="dashboard-container">
+        <div className="dashboard-sidebar">
+          <UserMenu />
         </div>
-        <div className="col-md-9">
+        <div className="dashboard-content">
           <div className="card">
             <h3>{auth?.user?.name}</h3>
             <h3>{auth?.user?.email}</h3>
